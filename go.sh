f518273470b316c1c4126d771f2a509720a66b8a
Angelo Dureghello <angelo@sysam.it>
2020-09-04 14:53:07 +0200

    fix for new erg name
    Signed-off-by: Angelo Dureghello <angelo@sysam.it>

--- go.sh
+++ go.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Sysam snmdist - CLFS based simple no-mmu rootfs generator
-
-# Toolchain setup into scripts s0
+# Sysam ERG - embedded rootfs generator
 
 export CMD_LINE="console=ttyS0,115200 root=/dev/ram0 rw rootfstype=ramfs rdinit=/sbin/init devtmpfs.mount=1"
 export INITRAMFS="../snmdist/targetfs"
@@ -10,17 +8,24 @@ DIR_DL=./downloads
 DIR_BUILD=./build
 
 function err {
-        echo -e "\x1b[1;31m+++ "$1"\x1b[0m"
-        echo -e "\x1b[31;1;5m+++ there are errors !\x1b[0m"
-        exit 1
+	echo -e "\x1b[1;31m+++ "$1"\x1b[0m"
+	echo -e "\x1b[31;1;5m+++ there are errors !\x1b[0m"
+	exit 1
 }
 
 function inf {
-        echo -e "\x1b[1;33m--- "$1"\x1b[0m"
+	echo -e "\x1b[1;33m--- "$1"\x1b[0m"
 }
 
 function msg {
-        echo -e "\x1b[1;34m--- "$1"\x1b[0m"
+	echo -e "\x1b[1;34m--- "$1"\x1b[0m"
+}
+
+function welcome {
+	echo -e "\x1b[36;1mHello, welcome to erg !\x1b[0m"
+	echo -e "\x1b[35;1merg v.${erg_version} Copyright (C) 2017 Angelo Dureghello - Sysam\x1b[0m"
+	echo -e "\x1b[37;1mPress enter to start ...\x1b[0m"
+	read -p ""
 }
 
 function step {
@@ -68,41 +73,27 @@ function build_pkg {
 	make distclean
 	./configure
 	echo "CFLAGS+=${PKG_CFLAGS}" >> Config
-	make ARCH="${CLFS_ARCH}" CROSS_COMPILE="${CLFS_CROSS}" EXTRA_CFLAGS="${PKG_CFLAGS}" V=1 SKIP_STRIP="y"
+	make ARCH="${ERG_ARCH}" CROSS_COMPILE="${ERG_CROSS}" EXTRA_CFLAGS="${PKG_CFLAGS}" V=1 SKIP_STRIP="y"
 	cd -
 }
 
-msg "Greetings, you are using Sysam snmdist, a simple embedded distribution !"
-msg "Copyright (C) 2017 Angelo Dureghello - Sysam"
-msg "Please visit http://www.sysam.it for further details"
-msg "Press enter to start ..."
-read -p ""
+source version
+
+welcome
 
 msg "Please select a cpu ..."
-echo "1. mcf54415 flat  (-mcpu=54418)"
-echo "2. mcf5475        (-mcpu=5475,-m5200)"
-echo "3. mcf5407        (-mcpu=5407,-m5200)"
-echo "4. mcf5307 flat   (-mcpu=5307,-m5307)"
-echo "5. mcf537x        (-mcpu=537x,-m5200)"
-echo "6. mcf528x        (-mcpu=528x,-m5307)"
-echo "7. mcf5271        (-mcpu=5271,-m5307)"
-echo "8. mcf5272        (-mcpu=5272,-m5307)"
-echo "9. mcf5275        (-mcpu=5275,-m5307)"
-echo "a. mcf523x        (-mcpu=523x,-m5307)"
-echo "b. mcf5253        (-mcpu=5253,-m5200)"
-echo "c. mcf5249        (-mcpu=5249,-m5200)"
-echo "d. mcf5206        (-mcpu=5206,-m5200)"
-echo "e. mcf5206e       (-mcpu=5206e,-m5200)"
-echo "f. mcf520x        (-mcpu=5208,-m5200)"
-
+echo
+echo "1. m68k  mcf54415 flat  (-mcpu=54418)"
+echo "2. m68k  mcf5307 flat   (-mcpu=5307,-m5307)"
+echo
 read -n 1 c
 
 case "$c" in
 	1)
-	clfs_cpu="-mcpu=54418"
+	erg_cpu="-mcpu=54418"
 	;;
-	4)
-	clfs_cpu="-mcpu=5307,-m5307"
+	2)
+	erg_cpu="-mcpu=5307,-m5307"
 	;;
 	*)
 	echo "not still implemented"
@@ -110,12 +101,15 @@ case "$c" in
 	;;
 esac
 
-echo -e "\n"
-step "Selecting cpu ... "
-step_done
+echo
 
 step "Setting up environment ... "
-source scripts/s00-setup.script
+source ./configs/environment
+step_done
+
+step "Reading configurations ..."
+source ./configs/cross-toolchain
+source ./configs/hostname
 step_done
 
 step "Preparing targetfs tree ... "
@@ -129,9 +123,6 @@ step_done
 step "Creating users, groups, passwd ... "
 source scripts/s02-groups-passwd.script
 step_done
-
-#-rw-r--r-- 1 angelo angelo  178 nov  5 01:10 s3-inst-libgcc.script
-#-rw-r--r-- 1 angelo angelo 3072 nov  5 10:00 s4-mdev.script
 
 step "Creating profile ... "
 source scripts/s05-profile.script
@@ -158,8 +149,8 @@ msg "Target fs created successfully."
 step "Configuring and installing busybox ..."
 cd sources/busybox-1.28.1/
 make clean
-make ARCH="${CLFS_ARCH}" menuconfig
-make ARCH="${CLFS_ARCH}" CROSS_COMPILE="${CLFS_CROSS}" EXTRA_CFLAGS="${clfs_cpu}" EXTRA_LDFLAGS="${clfs_cpu}" V=1 SKIP_STRIP="y" CONFIG_PREFIX="${CLFS}/targetfs" install
+make ARCH="${ERG_ARCH}" menuconfig
+make ARCH="${ERG_ARCH}" CROSS_COMPILE="${ERG_CROSS}" EXTRA_CFLAGS="${erg_cpu}" EXTRA_LDFLAGS="${erg_cpu}" V=1 SKIP_STRIP="y" CONFIG_PREFIX="${CLFS}/targetfs" install
 cd -
 
 step "Configuring and installing other tools ..."
