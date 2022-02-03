583d725a6e5367c0bb81b91c8675fd8ef75be033
Angelo Dureghello <angelo@sysam.it>
2022-02-03 07:40:11 +0100

    stmark2: fix wifi startup script
    Signed-off-by: Angelo Dureghello <angelo@sysam.it>

--- extra-files/stmark2/root/wifiup.sh
+++ extra-files/stmark2/root/wifiup.sh
@@ -6,6 +6,20 @@
 # WAKE os T1IN/PD0 8 * 4 + 0 = 32
 #
 
+printf "waking up first ... "
+
+if [ ! -e /sys/class/gpio/gpio32 ]; then
+        echo 32 > /sys/class/gpio/export
+fi
+
+# Wake if necessary
+echo out > /sys/class/gpio/gpio32/direction
+echo 1 > /sys/class/gpio/gpio32/value
+
+sleep 1
+
+echo "done"
+
 if [ ! -e /sys/class/gpio/gpio47 ]; then
         echo 47 > /sys/class/gpio/export
 fi
@@ -14,21 +28,12 @@ echo out > /sys/class/gpio/gpio47/direction
 
 printf "resetting ... "
 
+# Reset is transition of PD-n pin from low to high
 echo 1 > /sys/class/gpio/gpio47/value
 echo 0 > /sys/class/gpio/gpio47/value
+sleep 1
 echo 1 > /sys/class/gpio/gpio47/value
 
 echo "done"
 
-printf "waking up ... "
 
-if [ ! -e /sys/class/gpio/gpio32 ]; then
-        echo 32 > /sys/class/gpio/export
-fi
-
-echo out > /sys/class/gpio/gpio32/direction
-
-echo 0 > /sys/class/gpio/gpio32/value
-echo 1 > /sys/class/gpio/gpio32/value
-
-echo "done"
